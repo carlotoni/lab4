fcdccb7fb78e2507e3441088033c1e15917291d8
tonihe <tonihe7@gmail.com>
2019-01-29 12:14:48 -0800

    merge me pls

---
+++ devstarter/app/Models/students.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Student extends Model {
+
+    protected $data = [
+
+        'toni' => ['id' => 'toni he', 'description' => 'classified', 'class' => 'alpaca']
+        
+
+    ];
+
+}
+
+
